54e21e1d8a1b7808dd0c23c5a45a52b92c71d535
hzl <709493928@qq.com>
2019-05-28 13:37:40 +0800

    111

--- cosmetology/src/App.vue
+++ cosmetology/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <!--<router-view/>-->
+    <section class="el-container is-vertical">
+      <header class="el-header" style="height: 60px;">Header</header>
+      <section class="el-container">
+        <aside class="el-aside" style="width: 200px;">Aside</aside>
+        <main class="el-main">Main</main>
+      </section>
+    </section>
   </div>
 </template>
 
 <script>
+  import leftNav from './components/leftNav'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    leftNav
+  }
 }
 </script>
 
--- cosmetology/src/router/index.js
+++ cosmetology/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import leftNav from '@/components/leftNav'
 
 Vue.use(Router)
 
@@ -8,9 +8,9 @@ export default new Router({
   mode:"history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/leftNav',
+      name: 'leftNav',
+      component: leftNav
     }
   ]
 })
