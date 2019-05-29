2a82c1e36c0f61aa0f96029dd36a83064c3f0df9
hzl <709493928@qq.com>
2019-05-29 11:19:13 +0800

    new

---
+++ cosmetology/src/components/VipManage/Integral.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>积分规则</div>
+</template>
+
+<script>
+    export default {
+        name: "Integral"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ cosmetology/src/components/VipManage/VipLevel.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>会员级别设置</div>
+</template>
+
+<script>
+    export default {
+        name: "VipLevel"
+    }
+</script>
+
+<style scoped>
+
+</style>
