1d2368839b021a811d5a6679813543cc12398859
hzl <709493928@qq.com>
2019-06-01 11:27:28 +0800

    搜索组件、表格组件、地址组件

--- cosmetology/src/main.js
+++ cosmetology/src/main.js
@@ -14,10 +14,72 @@ Vue.prototype.$axios = Axios;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 /* eslint-disable no-new */
-new Vue({
+var vm=new Vue({
   el: '#app',
   router,
   store,//使用store
   components: { App },
   template: '<App/>',
 });
+// 拦截器
+//设置loading
+let loading;
+function startLoading() {
+  loading = Vue.prototype.$loading({
+    lock: true,
+    text: "Loading...",
+    background: "transparent"
+  });
+}
+function endLoading() {
+  loading.close();
+}
+// 拦截Axios发起的所有请求，给请求添加加载中
+Axios.interceptors.request.use(
+  config => {
+    startLoading(); // 显示loading组件
+    // console.log(store.getters.getToken);
+    // console.log("in request");
+    // if (store.getters.getToken) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
+      // console.log("request"+store.getters.getToken);
+      // config.headers['Authorization'] = store.getters.getToken;
+    // }else{
+      // config.headers['Authorization'] = '';
+    // }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  });
+
+//请求完成
+Axios.interceptors.response.use((response) => {
+  // 所有请求完成后都要执行的操作
+ endLoading();
+  return response;
+}, function (err) {
+  if (err.response) {
+    switch (err.response.status) {
+      case 401:
+        // 返回 401 清除token信息并跳转到登录页面
+        store.dispatch("logout");
+        router.replace({
+          path: 'login',
+          query: { redirect: router.history.current.fullPath }
+        });
+        break;
+      case 402:
+        store.dispatch("logout");
+        router.replace({
+          path: 'login',
+          query: { redirect: router.history.current.fullPath }
+        });
+        break;
+      case 403:
+       //操作
+    }
+  }
+  return Promise.reject(err);
+});
+
+
