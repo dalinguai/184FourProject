e281a81d9d2df060e900515156ac34b48ee79de8
ht <773320186@qq.com>
2019-06-10 22:19:36 +0800

    会员接口修改237

--- cosmetology/src/api/vipManage.js
+++ cosmetology/src/api/vipManage.js
@@ -3,39 +3,39 @@
 * */
 
 export default {
-  vipListAll: "http://172.17.1.236:8080/customer/selectByVipNo",//获取所有会员列表
-  vipListPage: "http://172.17.1.236:8080/customer/selectAllVip",//获取会员当前页面列表
-  searchVip: "http://172.17.1.236:8080/customer/selectByVipLike",//搜索会员信息
-  customerListPage:"/static/vipList-H.json",//获取当前页面散客列表
-  AddressList:"/static/Address.json",//获取省市区
+  vipListAll: "http://172.17.1.237:8080/customer/selectByVipNo",//获取所有会员列表
+  vipListPage: "http://172.17.1.237:8080/customer/selectAllVip",//获取会员当前页面列表
+  searchVip: "http://172.17.1.237:8080/customer/selectByVipLike",//搜索会员信息
+  customerListPage: "/static/vipList-H.json",//获取当前页面散客列表
+  AddressList: "/static/Address.json",//获取省市区
   // addVip:"http://5cec9881b779120014b4974f.mockapi.io/demo/VipList",//添加会员信息
-  addVip:"http://172.17.1.237:8080/customer/insertCustomer",
+  addVip: "http://172.17.1.237:8080/customer/insertCustomer",
   vipAffair: "/static/vipAffairDataOne.json",//获取会员事务相关信息
   // vipAffairComes: "/static/vipAffairDataTwo.json",//发送充值的金额
   //获取员工个人的所有疗程信息
-  vipAffairComes:"http://172.17.1.236:8080/customer/selectCourse",//获取疗程信息
+  vipAffairComes: "http://172.17.1.237:8080/customer/selectCourse",//获取疗程信息
   // vipAffairComesAdd: "/static/vipAffairDataThree.json",//补增获取疗程信息
   //点击请求对应的不增疗程项目
-  vipAffairComesAdd: "http://172.17.1.236:8080/customer/selectTreatment",
+  vipAffairComesAdd: "http://172.17.1.237:8080/customer/selectTreatment",
   //点击保存增加的补增疗程信息
-  insertTreatment:"http://172.17.1.236:8080/customer/insertTreatment",
-  VipsendMessage:"/static/sendMessage.json",//获取短信信息
-  VipsendHistory:"/static/sendHistory.json",//获取短信历史
+  insertTreatment: "http://172.17.1.237:8080/customer/insertTreatment",
+  VipsendMessage: "/static/sendMessage.json",//获取短信信息
+  VipsendHistory: "/static/sendHistory.json",//获取短信历史
   //会员预约
   // CustomerBooking:"/static/CustomersBooking.json",//获取预约详情
   //会员预约查询
-  CustomerBooking:"http://172.17.1.236:8080/Reservation/selectByDate",
+  CustomerBooking: "http://172.17.1.237:8080/Reservation/selectByDate",
   //添加预约
-  CustomerBookingAdd:"http://172.17.1.236:8080/Reservation/insertByDate",
+  CustomerBookingAdd: "http://172.17.1.237:8080/Reservation/insertByDate",
   //会员删除预约
-  CustomerBookingUpdate:"http://172.17.1.236:8080/Reservation/updateByDate",
-  sendMessageToVip:"http://172.17.1.237:8080/customer/insertNote" ,//向服务器发送短信
-  ViewConsumptionRecords:"http://172.17.1.236:8080/customer/selectConsume",//查询会员的商品消费详细信息
-  ViewTreatmentRecord:"http://172.17.1.236:8080/customer/selectAllCourse",//查询会员的疗程消费信息
+  CustomerBookingUpdate: "http://172.17.1.237:8080/Reservation/updateByDate",
+  sendMessageToVip: "http://172.17.1.237:8080/customer/insertNote",//向服务器发送短信
+  ViewConsumptionRecords: "http://172.17.1.237:8080/customer/selectConsume",//查询会员的商品消费详细信息
+  ViewTreatmentRecord: "http://172.17.1.237:8080/customer/selectAllCourse",//查询会员的疗程消费信息
 //  获取积分规则
-  VipIntegral:'http://172.17.1.237:8080/integral/select',
+  VipIntegral: 'http://172.17.1.237:8080/integral/select',
 //  充值提交确认
-    VipRecharge:'http://172.17.1.237:8080/VipRecharge/insert',
+  VipRecharge: 'http://172.17.1.237:8080/VipRecharge/insert',
   //会员消费排行
-  VipConsume:"http://172.17.1.237:8080/customer/selectRanking",
+  VipConsume: "http://172.17.1.237:8080/customer/selectRanking",
 }
